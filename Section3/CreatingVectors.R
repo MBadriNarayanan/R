9eaec47f6aabfddd8a06df33ddf7f4b762e64daf
M Badri Narayanan <58718023+MBadriNarayanan@users.noreply.github.com>
2020-07-08 01:05:59 +0530

    Fundamentals of R

---
+++ Section3/CreatingVectors.R
@@ -0,0 +1,51 @@
+MyFirstVector <- c(3L,45L,56L,732L)
+
+MyFirstVector
+
+is.numeric(MyFirstVector)
+
+is.integer(MyFirstVector)
+
+is.double(MyFirstVector)
+
+Vector2 <- c(1,2,3,4,5)
+
+Vector2
+
+is.numeric(Vector2)
+
+is.integer(Vector2)
+
+is.double(Vector2)
+
+Vector3 <- c("a","b","c",7)
+
+Vector3
+
+is.character(Vector3)
+
+is.numeric(Vector3)
+
+
+seq(1,15) # Sequence
+
+seq(1,15,2)
+
+z <- seq(1,15,4)
+
+z
+
+rep(3,10)
+
+d <- rep(3,15)
+
+
+rep('a',5)
+
+x <- c(80,20)
+
+x
+
+y <- rep(x , 10)
+
+y
---
+++ Section3/Functions.R
@@ -0,0 +1,31 @@
+rnorm(n=5,mean=10,sd=8)
+
+x <- c(1,2,3,4,5)
+
+A <- seq(from=10,to=20,along.with=x)
+
+seq(from=10,to=20,length.out=2)
+
+seq(from=10,to=20,by=2)
+
+rep(5,10)
+
+rep(5:7,each=10)
+
+# print()
+
+# is.numeric()
+
+# is.integer()
+
+# is.double()
+
+# is.character()
+
+# typeof()
+
+B <- sqrt(A)
+
+B
+
+# paste()
